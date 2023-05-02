9b3101e8bb1fbbc1ec1355b7456ac3dd5aeba583
Radko99 <sob_rad@wp.pl>
2023-05-02 18:55:57 +0200

    Data wrangling and basic EDA

--- stock_predictions.py
+++ stock_predictions.py
@@ -1,9 +1,9 @@
-# Loading libraries
 import pandas as pd
 import numpy as np
 
-market = pd.read_csv("Historyczne ceny AAPL.csv")
+market = pd.read_csv("AAPL Historical Data.csv", decimal=",")
 market = pd.DataFrame(market)
+
 
 # Market first 10 rows
 print(market.head(10))
@@ -14,6 +14,21 @@ print(market.shape)
 # Market data frame types
 print(market.dtypes)
 
+## Changing col name "Vol." to be more usefull
+market = market.rename(columns = {"Vol.":"Vol"})
+
+market[['Vol', "Unit"]] = market.Vol.str.split("M", expand = True)
+market = market.drop(columns = ["Unit"])
+
+print(market.head(10))
+
 # Market data frame missing values
-print(market.isnull().sum())
-market.info()
+print(market.isnull().sum().sort_values(ascending = False))
+
+# Convert data
+
+
+## Drop unecessary cols
+# market.drop
+
+
